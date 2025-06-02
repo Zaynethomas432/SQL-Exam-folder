905d4b1740f2583a2ce2359974eb1cfaf6a771db
Zaynethomas432 <zaynethomas@riccarton.school.nz>
2025-06-03 09:11:15 +1200

    added (NVIDIA, AMD, Intel) to the company input so the user knows what there are , also fixed some typos in the names of the colums

--- graphics_interface.py
+++ graphics_interface.py
@@ -65,3 +65,6 @@ while menu_choice != 'Z':
          print_query('good GB cards')   
     if menu_choice == 'E':
          print_query('top 10') 
+    if menu_choice == 'F':
+         company = input("What company's graphics cards do you want to see?(NVIDIA, AMD, Intel) ")
+         print_parameter_query("graph_card, processor, cores, tmus, rops, mem_size_gb, bandwidth_gbs, clock_spd_mhz,boost_spd_mhz, fp32_tflops, price", "manufacturer = ? ORDER BY fp32_tflops DESC",company)
